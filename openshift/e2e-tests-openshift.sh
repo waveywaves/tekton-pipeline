2c839705a75d4cae874c5cd8ef9738d256c817c1
Vincent Demeester <vdemeest@redhat.com>
2019-04-05 14:35:54 +0200

    Add ldflags to skip instead of failing in case of missing KO_DOCKER_REPO
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

--- openshift/e2e-tests-openshift.sh
+++ openshift/e2e-tests-openshift.sh
@@ -64,7 +64,7 @@ function create_test_namespace(){
 
 function run_go_e2e_tests(){
   header "Running Go e2e tests"
-  go_test_e2e ./test -timeout=20m --kubeconfig $KUBECONFIG || return 1
+  go_test_e2e -ldflags '-X github.com/tektoncd/pipeline/test.missingKoFatal=false' ./test -timeout=20m --kubeconfig $KUBECONFIG || return 1
 }
 
 function run_yaml_e2e_tests() {
