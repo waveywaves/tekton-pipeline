openshift/tektoncd-pipeline

Adds OpenShift monitoring config
In order to enable cluster-monitoring prometheus
to scrape the openshift pipeline metrics it requires
certain configuration such as "ServiceMonitor" and
list "pods, service etc" roles/rolebinding. This configuration can be
done through operator but it would create additional segway
and increase delta between upstream and downstream operator.

Instead, this patch alters `generate-release.yaml` to append
above mentioned configuration in `release.yaml`.

openshift/e2e-tests-openshift.sh
@@ -34,8 +34,7 @@ function install_tekton_pipeline() {
 }
 
 function create_pipeline() {
-  resolve_resources config/ tekton-pipeline-resolved.yaml "nothing" $OPENSHIFT_REGISTRY/$OPENSHIFT_BUILD_NAMESPACE/stable
-
+  generate_pipeline_resource tekton-pipeline-resolved.yaml $OPENSHIFT_REGISTRY/$OPENSHIFT_BUILD_NAMESPACE/stable
   oc apply -f tekton-pipeline-resolved.yaml
 }
 

openshift/release/generate-release.sh
@@ -13,4 +13,4 @@ else
     tag=$release
 fi
 
-resolve_resources config/ $output_file noignore $image_prefix $tag
+generate_pipeline_resource $output_file $image_prefix $tag

waveywaves/tekton-pipeline

openshift/e2e-tests-openshift.sh
@@ -34,8 +34,7 @@ function install_tekton_pipeline() {
 }
 
 function create_pipeline() {
-  resolve_resources config/ tekton-pipeline-resolved.yaml "nothing" $OPENSHIFT_REGISTRY/$OPENSHIFT_BUILD_NAMESPACE/stable
-
+  generate_pipeline_resources tekton-pipeline-resolved.yaml $OPENSHIFT_REGISTRY/$OPENSHIFT_BUILD_NAMESPACE/stable
   oc apply -f tekton-pipeline-resolved.yaml
 }
 

openshift/release/generate-release.sh
@@ -13,4 +13,4 @@ else
     tag=$release
 fi
 
-resolve_resources config/ $output_file noignore $image_prefix $tag
+generate_pipeline_resources $output_file $image_prefix $tag